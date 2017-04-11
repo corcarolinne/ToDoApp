8ba05fcb91d4990fd2fc7094f11fb2f60c0139d2
corcarolinne <carolinnemcordeiro@gmail.com>
2017-04-11 14:49:22 -0300

    Some changes on the folders and first Hello World

---
+++ src/components/presentional/list.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default List;
